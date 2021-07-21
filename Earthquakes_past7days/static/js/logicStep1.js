8b076103b0f4f9dc791c9fbeb1f1cd636885c10f
Nicole Housekeeper <nicolehousekeeper@Nicoles-MacBook-Air.local>
2021-07-20 22:03:12 -0400

    Add earthquake data points.:

--- Earthquakes_past7days/static/js/logicStep1.js
+++ Earthquakes_past7days/static/js/logicStep1.js
@@ -20,34 +20,18 @@ let baseMaps= {
 
 // Create the map object with a center and zoom level.
 let map = L.map("mapid", {
-    center: [43.7, -79.3],
-    zoom: 11,
+    center: [39.5, -98.5],
+    zoom: 3,
     layers: [streets]
 });
 
 //Pass map layers into layers control and add the layers control to the map
 L.control.layers(baseMaps).addTo(map);
 
-// Accessing the airport GeoJSON URL
-let torontoHoods = "https://raw.githubusercontent.com/nikki4944/Mapping_Earthquakes/Mapping_GeoJSON_Polygons/torontoNeighborhoods.json";
-
-// Create a style for the lines
-let myStyle= {
-    color: "blue",
-    weight: 1,
-    fillColor: "yellow"
-}
-
-//Grabbing GeoJSON data
-d3.json(torontoHoods).then(function(data) {
-    console.log(data);
-    //Create GeoJSON layer with the retrieved data
-L.geoJson(data, {
-    style: myStyle,
-    onEachFeature: function(feature, layer) {
-        layer.bindPopup("<h3> Neighborhood: " + feature.properties.AREA_NAME + "</h3>");
-    }
-}).addTo(map);
+// Retrieve the earthquake GeoJSON data.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJson(data).addTo(map);
 });
 
 
